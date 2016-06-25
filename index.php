2e0b646bb1b1a96fbc3e2c8cb00aa3605d6bbd9f
bank <zjubank@gmail.com>
2016-06-26 02:23:15 +0800

    login

--- index.php
+++ index.php
@@ -14,41 +14,12 @@
     <title>首页</title>
 
     <!-- Bootstrap -->
-  	<link rel="stylesheet" href="./css/bootstrap.css">
-  	<link rel="stylesheet" href="./css/bootstrap-theme.css">
+  	<link href="./css/bootstrap.css" rel="stylesheet" >
+  	<link href="./css/bootstrap-theme.css" rel="stylesheet">
   </head>
 
   <body>
 
-    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Project name</a>
-        </div>
-        <div id="navbar" class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-
-    <div class="container">
-
-      <div class="starter-template">
-        <h1>Bootstrap starter template</h1>
-        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
-      </div>
-
-    </div><!-- /.container -->
     <script src="./js/jquery.js"></script>
     <script src="./js/bootstrap.min.js"></script>
   </body>
--- login.php
+++ login.php
@@ -2,39 +2,38 @@
 <html lang="zh-cn">
 
 <head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
 	<title>登录</title>
-	<link href="./css/bootstrap.min.css" rel="stylesheet">
-	<link href="./css/bootstrap-theme.min.css" rel="stylesheet">
+
+	<!-- Bootstrap -->
+	<link href="./css/bootstrap.css" rel="stylesheet">
+	<link rel="stylesheet" href="./css/bootstrap-theme.css">
+	<link href="./css/signin.css" rel="stylesheet">
+
 </head>
 
 <body>
+	<div class="container">
 
-<div class="container-fluid">
-	<div class="row-fluid">
-		<div class="span12">
-			<form class="form-horizontal">
-				<div class="control-group">
-					 <label class="control-label" for="inputEmail">邮箱</label>
-					<div class="controls">
-						<input id="inputEmail" type="text" />
-					</div>
-				</div>
-				<div class="control-group">
-					 <label class="control-label" for="inputPassword">密码</label>
-					<div class="controls">
-						<input id="inputPassword" type="password" />
-					</div>
-				</div>
-				<div class="control-group">
-					<div class="controls">
-						 <label class="checkbox"><input type="checkbox" /> Remember me</label> <button type="submit" class="btn">登录</button>
-					</div>
-				</div>
-			</form>
-		</div>
-	</div>
-</div>
+	 <form class="form-signin" role="form" action="logincheck.php" method="post">
+		 <h2 class="form-signin-heading">Please sign in</h2>
+		 <label for="inputEmail" class="sr-only">Email address</label>
+		 <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+		 <label for="inputPassword" class="sr-only">Password</label>
+		 <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
+		 <div class="checkbox">
+			 <label>
+				 <input type="checkbox" value="remember-me"> Remember me
+			 </label>
+		 </div>
+		 <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+	 </form>
 
+ </div>
+ <!-- <script src="./js/ie10-viewport-bug-workaround.js"></script> -->
 </body>
 
 </html>
