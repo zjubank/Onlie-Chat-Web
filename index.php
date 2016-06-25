b5e7723080e926c7e82b86ab2d1b18e9683bd749
bank <zjubank@gmail.com>
2016-06-25 22:56:00 +0800

    fix nav-problem

--- index.php
+++ index.php
@@ -4,17 +4,53 @@
 
 <?php
   require 'dbconnect.php';
- ?>
-<head>
-	<title>首页</title>
-  
-	<link href="./css/bootstrap.min.css" rel="stylesheet">
-	<link href="./css/bootstrap-theme.min.css" rel="stylesheet">
-</head>
+?>
 
-<body>
-  <script src="./js/bootstrap.min.js"></script>
-  <script src="./js/jquery.js"></script>
-</body>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>首页</title>
+
+    <!-- Bootstrap -->
+  	<link rel="stylesheet" href="./css/bootstrap.css">
+  	<link rel="stylesheet" href="./css/bootstrap-theme.css">
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Project name</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div class="container">
+
+      <div class="starter-template">
+        <h1>Bootstrap starter template</h1>
+        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+      </div>
+
+    </div><!-- /.container -->
+    <script src="./js/jquery.js"></script>
+    <script src="./js/bootstrap.min.js"></script>
+  </body>
 
 </html>
