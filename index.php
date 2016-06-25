828627ad523078802e02d9a0b8f4292dd2adf135
bank <zjubank@gmail.com>
2016-06-26 04:30:57 +0800

    index and login/logout

--- index.php
+++ index.php
@@ -14,14 +14,71 @@
     <title>首页</title>
 
     <!-- Bootstrap -->
-  	<link href="./css/bootstrap.css" rel="stylesheet" >
-  	<link href="./css/bootstrap-theme.css" rel="stylesheet">
+  	<link href="./css/bootstrap.min.css" rel="stylesheet" >
+  	<!-- <link href="./css/bootstrap-theme.min.css" rel="stylesheet"> -->
+    <link href="./css/cover.css" rel="stylesheet">
+    <script src="./js/ie-emulation-modes-warning.js"></script>
   </head>
 
   <body>
 
+    <div class="site-wrapper">
+
+      <div class="site-wrapper-inner">
+        <div class="cover-container">
+
+          <div class="masthead clearfix">
+            <div class="inner">
+              <h3 class="masthead-brand">Chat</h3>
+              <nav>
+                <ul class="nav masthead-nav">
+                  <li class="active"><a href="#">首页</a></li>
+                  <li><a href="chat.php">开始Chat</a></li>
+                  <li><a href="about.php">关于Chat</a></li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+
+          <div class="inner cover">
+            <h1 class="cover-heading">Let Chat！</h1>
+            <p class="lead"></p>
+            <p class="lead">Chat是一个基于Browser/Server的聊天工具</p>
+            <p class="lead">登录或注册，并与你的好友开始聊天</p>
+            <!-- <h1 class="cover-heading">ラブライブ！</h1>
+            <p class="lead">IF WE WANT TO PROTECT</p>
+            <p class="lead">OUT BELOVED SCHOOL,</p>
+            <p class="lead">WE NEED TO DO WHAT WE CAN ____</p>
+            <p class="lead">WE NEED TO BECOME IDOLS!</p>
+            <p class="lead">NOW THEIR "SCHOOL IDOL PROJECT"</p>
+            <p class="lead">BEGINS TO MAKE</p>
+            <p class="lead">THEIR DREAMS COME TRUE!</p> -->
+
+            <p class="lead">
+              <a href="<?php if(!$_SESSION["login"]==1) echo "login.php"; else echo "chat.php"; ?>" class="btn btn-lg btn-primary">
+                <?php if(!$_SESSION["login"]==1) echo "登录"; else echo "开始chat"; ?>
+              </a>
+              <a href="<?php if(!$_SESSION["login"]==1) echo "signin.php"; else echo "logoutcheck.php"; ?>" class="btn btn-lg btn-default">
+                <?php if(!$_SESSION["login"]==1) echo "注册"; else echo "注销"; ?>
+              </a>
+            </p>
+
+          </div>
+
+          <div class="mastfoot">
+            <div class="inner">
+              <p>By <a href="http://zjubank.com">@zjubank</a>.</p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+    </div>
+
     <script src="./js/jquery.js"></script>
     <script src="./js/bootstrap.min.js"></script>
+    <script src="./js/ie10-viewport-bug-workaround.js"></script>
   </body>
 
 </html>
--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
+<?php session_start(); ?>
 <html lang="zh-cn">
-
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,26 +10,42 @@
 
 	<!-- Bootstrap -->
 	<link href="./css/bootstrap.css" rel="stylesheet">
-	<link rel="stylesheet" href="./css/bootstrap-theme.css">
+	<!-- <link rel="stylesheet" href="./css/bootstrap-theme.css"> -->
 	<link href="./css/signin.css" rel="stylesheet">
 
 </head>
 
 <body>
+	<?php
+	if( $_SESSION["login"]==1 )
+	{
+		echo "<p class=\"text-center\">".$_SESSION["email"].":您已登录，即将自动跳转回首页</p>\n";
+		echo "<p class=\"text-center\">如果您的浏览器不支持自动跳转，请点击<a href=\"index.php\">这里</a></p>\n";
+		echo "<meta http-equiv=\"refresh\" content=\"3;url=index.php\">";
+		exit();
+	}
+	?>
 	<div class="container">
 
 	 <form class="form-signin" role="form" action="logincheck.php" method="post">
-		 <h2 class="form-signin-heading">Please sign in</h2>
-		 <label for="inputEmail" class="sr-only">Email address</label>
-		 <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-		 <label for="inputPassword" class="sr-only">Password</label>
+		 <h2 class="form-signin-heading">请登录</h2>
+
+		 <div class="form-group">
+			 <label for="inputEmail">邮箱地址</label>
+			 <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+	 	 </div>
+
+		 <div class="form-group">
+		 <label for="inputPassword">密码</label>
 		 <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
+	 	 </div>
+
 		 <div class="checkbox">
 			 <label>
-				 <input type="checkbox" value="remember-me"> Remember me
+				 <input type="checkbox" value="remember-me"> 记住我
 			 </label>
 		 </div>
-		 <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+		 <button class="btn btn-lg btn-primary btn-block" type="submit">Log in</button>
 	 </form>
 
  </div>
