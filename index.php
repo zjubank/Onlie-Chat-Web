6182f3629d455e4b1719ffe0b33fb0c4b6931aad
bank <zjubank@gmail.com>
2016-06-25 20:55:56 +0800

    html head

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<?php session_start(); ?>
+<html lang="zh-cn">
+
+<head>
+	<title>首页</title>
+	<link href="./css/bootstrap.min.css" rel="stylesheet">
+	<link href="./css/bootstrap-theme.min.css" rel="stylesheet">
+</head>
+
+<body>
+  <script src="./js/bootstrap.min.js"></script>
+  <script src="./js/jquery.js"></script>
+</body>
+
+</html>
---
+++ login.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="zh-cn">
+
+<head>
+	<title>登录</title>
+	<link href="./css/bootstrap.min.css" rel="stylesheet">
+	<link href="./css/bootstrap-theme.min.css" rel="stylesheet">
+</head>
+
+<body>
+
+<div class="container-fluid">
+	<div class="row-fluid">
+		<div class="span12">
+			<form class="form-horizontal">
+				<div class="control-group">
+					 <label class="control-label" for="inputEmail">邮箱</label>
+					<div class="controls">
+						<input id="inputEmail" type="text" />
+					</div>
+				</div>
+				<div class="control-group">
+					 <label class="control-label" for="inputPassword">密码</label>
+					<div class="controls">
+						<input id="inputPassword" type="password" />
+					</div>
+				</div>
+				<div class="control-group">
+					<div class="controls">
+						 <label class="checkbox"><input type="checkbox" /> Remember me</label> <button type="submit" class="btn">登录</button>
+					</div>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
+
+</body>
+
+</html>
